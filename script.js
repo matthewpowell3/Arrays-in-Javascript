a775a645990c1193599d6e51b536d7cb78b188b3
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-03-10 21:03:52 -0500

    1 file modified

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 // Arrays in Javascript
+//          0       1         2
+var car = ['Hond', 'Bimma', 'Crown'];
+document.write('<h1>' + car + '</h1>');
 
+// too select one out of the rest you must first know the index which starts from xeron so honda is zero bimma is one
+
+/*
 var car1 = 'Honda';
 var car2 = 'Bimma';
 var car3 = 'Crown';
+
+document.write('<h1>' + car1 + '</h1>');
+document.write('<h1>' + car2 + '</h1>');
+document.write('<h1>' + car3 + '</h1>');
+ */
