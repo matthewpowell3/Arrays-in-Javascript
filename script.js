294c00f1938f63796897a8e997cca251535deb53
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-03-11 02:44:31 -0500

    1 file modified

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 // Arrays in Javascript
 //          0       1         2
 var car = ['Honda', 'Bimma', 'Crown'];
+
 // push add and pop removes
 car.push('Go cart');
 car.pop();
 car.pop();
+// car.sort()
+// sor it in order with numbers
+
+car.push(20);
+// use any data types
+
 car.push('Tesla');
+//replacing crown with tesla
 // it removes go cart
+
 // var vegetables = new Array('Carrrot', 'Green beans', 'Pepper');
-for (var i = 0; i < 4; i++) {
+for (var i = 0; i < car.length; i++) {
+  // car.length
+  // it is the length of the array
+  // you don;t have to constantly update length of the array with array length
+
   document.write('<h1>' + car[i] + '</h1>');
   // for loop will go from one to three
 }
